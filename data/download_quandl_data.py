4a602e7999ed62c29467eeff3deb74e59afdf554
sina <sinarahimzade.edu@gmail.com>
2022-07-16 16:07:37 +0430

    adding tse

--- data/download_quandl_data.py
+++ data/download_quandl_data.py
@@ -1,50 +1,19 @@
 import quandl
 from settings.default import ALL_QUANDL_CODES
 import datetime as dt
+import pytse_client as tse
 import argparse
 import os
 
 DEPTH = 1
 
-def main(api_key: str):
-    quandl.ApiConfig.api_key = api_key
 
-    if not os.path.exists(os.path.join("data", "quandl")):
-        os.mkdir(os.path.join("data", "quandl"))
+base_path = os.path.join("data", "tse")
 
-    for t in ALL_QUANDL_CODES:
-        print(t)
-        try:
-            data = quandl.get(
-                f"{t}{DEPTH}",
-                start_date="1988-01-01",
-            )
-        except BaseException as ex:
-            print(ex)
-        if ("Settle" in data.columns) and (data.index.min() <= dt.datetime(2015, 1, 1)):
-            data[["Settle"]].to_csv(
-                os.path.join("data", "quandl", f"{t.split('/')[-1]}.csv")
-            )
+def main(tickers : list):
+    tse.download(tickers, adjust= True, base_path=base_path, write_to_csv=True)
+    return
 
 
 if __name__ == "__main__":
-
-    def get_args():
-        """Download the Quandl data"""
-
-        parser = argparse.ArgumentParser(description="Download the Quandl data.")
-        parser.add_argument(
-            "api_key",
-            metavar="k",
-            type=str,
-            nargs="?",
-            help="quandl API key",
-        )
-
-        args = parser.parse_known_args()[0]
-
-        return (
-            args.api_key,
-        )
-    
-    main(*get_args())
+    main()
--- data/pull_data.py
+++ data/pull_data.py
@@ -10,8 +10,8 @@ from settings.default import PINNACLE_DATA_CUT, PINNACLE_DATA_FOLDER
 
 def pull_quandl_sample_data(ticker: str) -> pd.DataFrame:
     return (
-        pd.read_csv(os.path.join("data", "quandl", f"{ticker}.csv"), parse_dates=[0])
-        .rename(columns={"Trade Date": "date", "Date": "date", "Settle": "close"})
+        pd.read_csv(os.path.join("data", "quandl", ticker+'ت'+'.csv'), parse_dates=[0])
+        .rename(columns={'adjClose': 'close'})
         .set_index("date")
         .replace(0.0, np.nan)
     )
